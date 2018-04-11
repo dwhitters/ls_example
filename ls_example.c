6ea0a908f536665150968742d811302e8db496de
David Whitters <d.whitters@yahoo.com>
2018-04-11 16:28:41 -0400

    Done

--- ls_example.c
+++ ls_example.c
@@ -9,9 +9,9 @@
 
 int main(int argc, char * argv[])
 {
-    DIR *dirPtr;
-    struct dirent *entryPtr;
-    struct stat statBuf;
+	DIR *dirPtr;
+	struct dirent *entryPtr;
+	struct stat statBuf;
 
 	char arg[3] = {0};
 	char file_path[128] = {0};
@@ -23,13 +23,13 @@ int main(int argc, char * argv[])
 	/* There must be additional arguments. */
 	if(argc == 1)
 	{
-		perror("Must provide file path!");
+		printf("Must provide file path!\n");
 		exit(EXIT_FAILURE);
 	}
 	/* 
- 		We are assuming that there will either only by one argument
-		or no arguments.
-	*/
+	   We are assuming that there will either only by one argument
+	   or no arguments.
+	   */
 	if(argc == 3)
 	{
 		/* Copy the argument into our arg string if present. */
@@ -42,29 +42,66 @@ int main(int argc, char * argv[])
 	memcpy(file_path, argv[index], strlen(argv[index]));
 
 	/* Open the file path provided. */
-    if((dirPtr = opendir(file_path)) == NULL)
-    {
-	perror("Error on opening directory.");
-	exit(EXIT_FAILURE);
-    }
+	if((dirPtr = opendir(file_path)) == NULL)
+	{
+		printf("Error on opening directory.\n");
+		exit(EXIT_FAILURE);
+	}
 
 	/* Read all the entries in the file path. */
-    while ((entryPtr = readdir(dirPtr)))
-    {
-			printf("Name: %-20s", entryPtr->d_name);
-			printf("\t");
-
-			char file_to_search[256] = {0};
-			memcpy(file_to_search, file_path, strlen(file_path));
-			(void)strcat(file_to_search, "/");
-			(void)strcat(file_to_search, entryPtr->d_name);
-
-			if (stat(file_to_search, &statBuf) < 0) {
-				perror("huh?  there is ");
-				exit(1);
+	while ((entryPtr = readdir(dirPtr)))
+	{
+		if(arg_provided)
+		{
+			if(arg[1] == 'a')
+			{
+				printf("%s  ", entryPtr->d_name);
 			}
-			printf("Size: %llu\n", (unsigned long long)statBuf.st_size);
-    }
-    closedir(dirPtr);
-    return 0;
+			else if(arg[1] == 'n')
+			{
+				char file_to_search[256] = {0};
+				memcpy(file_to_search, file_path, strlen(file_path));
+				(void)strcat(file_to_search, "/");
+				(void)strcat(file_to_search, entryPtr->d_name);
+
+				if (stat(file_to_search, &statBuf) < 0) {
+					printf("huh?  there is \n");
+					exit(1);
+				}
+				printf("%llu\t", (unsigned long long)statBuf.st_uid);
+				printf("%llu\t", (unsigned long long)statBuf.st_gid);
+				printf("%llu\t", (unsigned long long)statBuf.st_size);
+				printf("%s\n", entryPtr->d_name);
+			}
+			else if(arg[1] == 'i')
+			{
+				char file_to_search[256] = {0};
+				memcpy(file_to_search, file_path, strlen(file_path));
+				(void)strcat(file_to_search, "/");
+				(void)strcat(file_to_search, entryPtr->d_name);
+
+				if (stat(file_to_search, &statBuf) < 0) {
+					printf("huh?  there is \n");
+					exit(1);
+				}
+				printf("%llu\t", (unsigned long long)statBuf.st_ino);
+				printf("%s\n", entryPtr->d_name);
+			}
+		}
+		else
+		{
+			/* Don't allow hidden files to be displayed. */
+			if(entryPtr->d_name[0] == '.')
+			{
+				continue;
+			}
+			printf("%s  ", entryPtr->d_name);
+		}
+	}
+
+	printf("\n");
+
+	/* Close the directory stream. */
+	closedir(dirPtr);
+	return 0;
 }
